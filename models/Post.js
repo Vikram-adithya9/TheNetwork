ae492d80c4585594093b3f3bfe8c76694844e3f8
Vikram-adithya9 <189125562+Vikram-adithya9@users.noreply.github.com>
2025-01-31 12:36:04 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const User = require('User');
+
 const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
